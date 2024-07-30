e5bdef0011583be20447b2680e612bef0e16e9d4
Masy Jordan <masyjordan@gmail.com>
2024-07-30 10:29:06 +0200

    update files and connection of the db

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,14 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors());
-
+/*
 app.use("/api/users", userRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/companies", companiesRoutes);
 app.use("/api/images", imageRoutes);
 app.use("/api/invoices", invoiceRoutes);
 app.use("/api/", miscRoutes);
-app.use("/api/types", typeRoutes);
+app.use("/api/types", typeRoutes);*/
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
